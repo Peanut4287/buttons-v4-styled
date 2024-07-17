459de2369503ddfd2425bd0082e73135b926c095
Peanut4287 <157473901+Peanut4287@users.noreply.github.com>
2024-07-17 13:08:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Button from "primevue/button";
+</script>
+
+<template>
+  <div class="flex flex-column gap-5">
+    <div class="flex align-items-end gap-1">
+      <Button icon="pi pi-check" label="Foo" size="small" />
+      <Button icon="pi pi-check" label="Foo" />
+      <Button icon="pi pi-check" label="Foo" size="large" />
+    </div>
+    <div class="flex align-items-end gap-1">
+      <Button icon="pi pi-check" label="Foo" rounded size="small" />
+      <Button icon="pi pi-check" label="Foo" rounded />
+      <Button icon="pi pi-check" label="Foo" rounded size="large" />
+    </div>
+    <div class="flex align-items-end gap-1">
+      <Button icon="pi pi-check" size="small" />
+      <Button icon="pi pi-check" />
+      <Button icon="pi pi-check" size="large" />
+    </div>
+    <div class="flex align-items-end gap-1">
+      <Button icon="pi pi-check" rounded size="small" />
+      <Button icon="pi pi-check" rounded />
+      <Button icon="pi pi-check" rounded size="large" />
+    </div>
+  </div>
+</template>
